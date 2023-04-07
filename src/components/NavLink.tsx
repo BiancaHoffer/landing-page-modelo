cd8f2090359ec05f08055e1c57acd024d4fea6c8
Bianca Hoffer <bianca.mhm1@gmail.com>
2023-04-07 21:06:11 +0100

    teste drawer

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -5,7 +5,7 @@ interface NavLinkProps {
 
 export function NavLink({ href, title, ...rest }: NavLinkProps) {
   return (
-    <a href={href} className='uppercase font-sans text-gray800 leading-[18.2px] hover:text-orange500 transition tracking-[0.7rem] text-[14px] xl:text-[12px] md:text-white md:hover:text-gray800 md:text-[18px]' {...rest}>
+    <a href={href} className='uppercase font-sans text-gray800 leading-[18.2px] hover:text-orange500 transition tracking-[0.7rem] text-[14px] xl:text-white xl:hover:text-gray800 xl:text-[18px] lg:hover:text-blue750' {...rest}>
       {title}
     </a>
   )
